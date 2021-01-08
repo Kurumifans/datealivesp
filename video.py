0f2c51be63e1b0e0b901f5fe2f0c243199ac1eab
DALFC <dalfc@users.noreply.github.com>
2021-01-08 23:28:42 +0700

    Fix no directory

--- video.py
+++ video.py
@@ -1,9 +1,10 @@
-import subprocess, sys
+import subprocess, sys, os
 
 server = sys.argv[1]
 
 with open(server + "/" + "video.txt", "r") as f:
 	files = f.read().split('\n')
 for file in files:
+	os.makedirs(os.path.dirname(file), exist_ok=True)
 	subprocess.call(['ffmpeg', '-y', '-i', server + "/" + file, "-c:v", "libx264", "-crf", "28", "-vf", "format=yuv420p", "-c:a", "copy", "dal_video/" + server + "/" + file])
 	# ffmpeg -y -i input.mp4 -c:v libx264 -crf 28 -vf format=yuv420p -c:a copy output.mp4
