1fd08b1fd202ec5a69e472cff7c34135ac7520c9
DALFC <dalfc@users.noreply.github.com>
2021-01-09 00:07:33 +0700

    Decrease compression rate

--- video.py
+++ video.py
@@ -9,5 +9,5 @@ except:
 	sys.exit(1) # No video files in this update
 for file in files:
 	os.makedirs(os.path.dirname("dal_video/" + server + "/" + file), exist_ok=True)
-	subprocess.call(['ffmpeg', '-y', '-i', server + "/" + file, "-c:v", "libx264", "-crf", "28", "-vf", "format=yuv420p", "-c:a", "copy", "dal_video/" + server + "/" + file])
+	subprocess.call(['ffmpeg', '-y', '-i', server + "/" + file, "-c:v", "libx264", "-crf", "23", "-vf", "format=yuv420p", "-c:a", "copy", "dal_video/" + server + "/" + file])
 	# ffmpeg -y -i input.mp4 -c:v libx264 -crf 28 -vf format=yuv420p -c:a copy output.mp4
