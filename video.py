8c3e99a7ec6e83c8b17110f4201803fd2255075b
DALFC <dalfc@users.noreply.github.com>
2021-01-18 18:37:50 +0700

    Changed workflow

--- video.py
+++ video.py
@@ -11,3 +11,11 @@ for file in files:
 	os.makedirs(os.path.dirname("dal_video/" + server + "/" + file), exist_ok=True)
 	subprocess.call(['ffmpeg', '-y', '-i', server + "/" + file, "-c:v", "libx264", "-crf", "23", "-vf", "format=yuv420p", "-c:a", "copy", "dal_video/" + server + "/" + file])
 	# ffmpeg -y -i input.mp4 -c:v libx264 -crf 28 -vf format=yuv420p -c:a copy output.mp4
+
+# Done converting
+subprocess.call(['git', 'stash'])
+subprocess.call(['git', 'pull'])
+subprocess.call(['git', 'stash', 'apply'])
+subprocess.call(['git', 'add', 'dal_video'])
+subprocess.call(['git', 'commit', '-F', server + '/version.txt'])
+subprocess.call(['git', 'push', 'origin', 'master'])
